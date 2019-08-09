f9dfa40ac69361e2b93bfd293fa1dd8c37e33e0b
Nathaniel Graff <nathaniel.graff@gmail.com>
2019-08-08 18:21:17 -0700

    Build gstreamer pipeline
    Signed-off-by: Nathaniel Graff <nathaniel.graff@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate gstreamer as gst;
+
+use gst::prelude::*;
+
+use std::env;
+use std::process::exit;
+
 fn main() {
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("{}: Please provide a video as argument", args[0]);
+		exit(1);
+	}
+	let inputfile = &args[1];
+
+    gst::init().unwrap();
+
+    let source = gst::ElementFactory::make("filesrc", Some("source"))
+    	.expect("Could not create playbin element");
+	let sink = gst::ElementFactory::make("appsink", Some("sink"))
+	    	.expect("Could not create appsink element");
+
+	let pipeline = gst::Pipeline::new(Some("my-pipeline"));
+
+	pipeline.add_many(&[&source, &sink]).unwrap();
+
+	source.set_property("location", &inputfile)
+		.expect("Can't set uri on source");
+	source.link(&sink).expect("Could not link source and sink");
+
+	pipeline.set_state(gst::State::Paused).expect("Unable to pause pipeline");
+        
     println!("Hello, world!");
 }
