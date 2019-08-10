d720e40da4bc7971b7b88081c58632087e70dd08
Nathaniel Graff <nathaniel.graff@gmail.com>
2019-08-10 14:15:23 -0700

    Play pipeline and render all frames
    Signed-off-by: Nathaniel Graff <nathaniel.graff@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
 	let inputfile = &args[1];
 
 	gst::init().unwrap();
-	//let main_loop = glib::MainLoop::new(None, false);
+	let main_loop = glib::MainLoop::new(None, false);
 	let pipeline = gst::Pipeline::new(None);
 
 	let width: usize = 1080 / 10;
@@ -70,14 +70,14 @@ fn main() {
 	 * sink pad of videoconvert. */
 	decode.connect_pad_added(move |_, src_pad| {
 		let pipeline = match pipeline_weak.upgrade() {
-            Some(pipeline) => pipeline,
-            None => return,
-        };
+			Some(pipeline) => pipeline,
+			None => return,
+		};
 
-        let convert = pipeline.get_by_name("vconvert").unwrap();
-        let convert_sink_pad = convert.get_static_pad("sink").unwrap();
+		let convert = pipeline.get_by_name("vconvert").unwrap();
+		let convert_sink_pad = convert.get_static_pad("sink").unwrap();
 
-        // Only link the pads once
+		// Only link the pads once
 		if ! ( src_pad.is_linked() || convert_sink_pad.is_linked() ) {
 			src_pad.link(&convert_sink_pad).unwrap();
 
@@ -89,14 +89,14 @@ fn main() {
 	});
 
 	// Set the pipeline state to paused to stay on a single frame
-	pipeline.set_state(gst::State::Paused).expect("Unable to pause pipeline");
+	pipeline.set_state(gst::State::Playing).expect("Unable to Play pipeline");
 
 	/*
 	 * Error and State Change Message Callbacks
 	 */
 
 	let bus = pipeline.get_bus().expect("Failed to get pipeline bus");
-	//let main_loop_clone = main_loop.clone();
+	let main_loop_clone = main_loop.clone();
 	bus.add_watch(move |_, msg| {
 		match msg.view() {
 			gst::MessageView::Error(err) => {
@@ -109,7 +109,7 @@ fn main() {
 
 				pipeline.set_state(gst::State::Null)
 					.expect("Unable to stop pipeline");
-				//main_loop_clone.quit();
+				main_loop_clone.quit();
 			}
 			gst::MessageView::StateChanged(state_changed) => {
 				let current_state = state_changed.get_current();
@@ -148,19 +148,25 @@ fn main() {
 		.dynamic_cast::<AppSink>()
 		.expect("Sink element is expected to be an appsink!");
 
-	let sample = appsink.pull_preroll().expect("Failed to pull appsink preroll");
-	let buffer = sample.get_buffer().expect("Failed to get buffer");
-	let map = buffer.map_readable().unwrap();
+	appsink.set_callbacks(
+		gst_app::AppSinkCallbacks::new()
+			.new_sample(move |appsink| {
+				let sample = appsink.pull_sample().expect("Failed to pull appsink preroll");
+				let buffer = sample.get_buffer().expect("Failed to get buffer");
+				let map = buffer.map_readable().unwrap();
 
-	// Convert the mapped buffer to a slice of u8
-	let samples: &[u8] = map.as_slice();
+				// Convert the mapped buffer to a slice of u8
+				let samples: &[u8] = map.as_slice();
 
-	for row in samples.chunks(3 * width) {
-		for pixel in row.chunks(3) {
-			print!("\x1b[38;2;{};{};{}m█\x1b[0m", pixel[0], pixel[1], pixel[2]);
-		}
-		print!("\n");
-	}
+				for row in samples.chunks(3 * width) {
+					for pixel in row.chunks(3) {
+						print!("\x1b[38;2;{};{};{}m█\x1b[0m", pixel[0], pixel[1], pixel[2]);
+					}
+					print!("\n");
+				}
 
-	//main_loop.run();
+				Ok(gst::FlowSuccess::Ok)
+			}).build());
+
+	main_loop.run();
 }
